83d837160b06408c2206ea8bb6ced4de864fc176
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2011-11-01 10:35:04 +0000

    Testing simple methods on ‘diabolical’ grid.

--- test.rb
+++ test.rb
@@ -4,3 +4,9 @@ solver.print
 solver.solve
 puts solver.solved?
 solver.print
+
+solver_diabolical = SudokuSolver.new "diabolical.sdk"
+solver_diabolical.print
+solver_diabolical.solve
+puts solver_diabolical.solved?
+solver_diabolical.print
