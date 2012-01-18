9a34f8e858a90a6f45ba6a9b2e1af7a024c82931
Arthur Reutenauer <arthur@hubsphere.com>
2012-01-18 19:09:23 +0000

    Cleaned up the bloody mess.

--- test.rb
+++
@@ -1,9 +0,0 @@
-require 'sudoku'
-grids = ["simple", "average", "hard", "expert", "diabolical"]
-grids.each do |name|
-  solver = SudokuSolver.new "#{name}.sdk"
-  solver.print
-  solver.solve
-  puts solver.solved?
-  solver.print
-end
