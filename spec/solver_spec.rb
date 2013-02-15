8b03bff1fc56ff486802d1db2d3520e8fb63ad7e
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-02-15 18:40:08 +0000

    Added spec for the class.

---
+++ spec/solver_spec.rb
@@ -0,0 +1,10 @@
+require File.expand_path(__FILE__, '../sudoku')
+
+describe "SudokuSolver" do
+  describe "#solve" do
+    it "solves the grid" do
+      # solver = SudokuSolver.new(File.join(__FILE__), '..', 'grids', 'guardian', '2423.sdk')
+      # expect { solver.solve }.to change(solver, :nb_cell_solved) by(81)
+    end
+  end
+end
