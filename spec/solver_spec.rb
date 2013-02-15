5f766cb2db2530d16a145ce17d08e2ce3ece9df1
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-02-15 18:49:58 +0000

    Test didn’t test anything; now works.

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,10 +1,12 @@
-require File.expand_path(__FILE__, '../sudoku')
+require File.expand_path('../lib.rb', File.dirname(__FILE__))
 
 describe "SudokuSolver" do
   describe "#solve" do
     it "solves the grid" do
-      # solver = SudokuSolver.new(File.join(__FILE__), '..', 'grids', 'guardian', '2423.sdk')
+      solver = SudokuSolver.new(File.expand_path('../grids/guardian/2423.sdk', File.dirname(__FILE__)))
       # expect { solver.solve }.to change(solver, :nb_cell_solved) by(81)
+      solver.solve
+      solver.nb_cell_solved.should == 81
     end
   end
 end
