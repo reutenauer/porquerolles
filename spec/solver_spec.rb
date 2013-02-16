93152ddc21b36c8e2fcfb76bc2dc0bbf77373f0d
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-02-16 15:43:49 +0000

    Added test for guess method to specs.

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,10 +2,17 @@ require File.expand_path('../lib.rb', File.dirname(__FILE__))
 
 describe "SudokuSolver" do
   describe "#solve" do
-    it "solves the grid" do
+    it "solves a couple of grids" do
       solver = SudokuSolver.new(File.expand_path('../grids/guardian/2423.sdk', File.dirname(__FILE__)))
       # expect { solver.solve }.to change(solver, :nb_cell_solved) by(81)
       solver.solve
+      solver.nb_cell_solved.should == 81
+
+      solver = SudokuSolver.new(File.expand_path('../grids/maman.sdk', File.dirname(__FILE__)))
+      solver.solve
+      solver.nb_cell_solved.should == 24
+
+      solver.solve(:method => :guess)
       solver.nb_cell_solved.should == 81
     end
   end
