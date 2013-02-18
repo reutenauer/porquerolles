450254bdfdf7dbe7682a06de2cd9aff84f6c2701
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-02-18 15:28:07 +0000

    Typographical changes in spec file.

--- spec/lib_spec.rb
+++ spec/lib_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path('../lib.rb', File.dirname(__FILE__))
 
 describe Block do
   describe '#place_single' do
-    it 'works' do
+    it "works" do
       solver = SudokuSolver.new
       grid = Grid.new(solver.parse_file(File.expand_path('../grids/guardian/2423.sdk', File.dirname(__FILE__))))
       block = grid.blocks.first
@@ -12,7 +12,7 @@ describe Block do
       end
     end
 
-    it 'places one value on one single values' do
+    it "places one value on one single values" do
       solver = SudokuSolver.new(File.expand_path('../grids/simple.sdk', File.dirname(__FILE__)))
       grid = solver.grid
       block = grid.blocks.last
@@ -35,19 +35,19 @@ describe Grid do
   context "with an empty grid" do
     let(:grid) { @grid = Grid.new }
 
-    describe ".row_of" do
+    describe "#row_of" do
       it "finds the row of one cell" do
         grid.row_of([2, 3]).should == grid.rows[2]
       end
     end
 
-    describe ".column_of" do
+    describe "#column_of" do
       it "finds the column on one cell" do
         grid.column_of([4, 6]).should == grid.columns[6]
       end
     end
 
-    describe ".block_of" do
+    describe "#block_of" do
       it "finds the block of some cells" do
         grid.block_of([6, 6]).should == grid.blocks[8]
         grid.block_of([4, 3]).should == grid.blocks[4]
@@ -65,8 +65,8 @@ describe Grid do
     end
   end
 
-  describe '#find_chains' do
-    it 'find a link' do
+  describe "#find_chains" do
+    it "find a link" do
       solver = SudokuSolver.new(File.expand_path('X-wing.sdk', File.dirname(__FILE__)))
       grid = solver.grid
       solver.solve
