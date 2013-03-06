b1580788d5bd40b6715657efbac8b5c6ead22fd9
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-03-05 20:54:59 -0500

    Some comments in the spec.

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,8 +4,10 @@ describe SudokuSolver do
   describe "#solve" do
     it "solves a couple of grids" do
       solver = SudokuSolver.new(File.expand_path('../grids/guardian/2423.sdk', File.dirname(__FILE__)))
-      # expect { solver.solve }.to change(solver, :nb_cell_solved) by(81)
+      # Not sure whether to test that.
+      # expect { solver.solve }.to change(solver, :nb_cell_solved) by(57)
       solver.solve
+      # TODO Matcher for that!
       solver.nb_cell_solved.should == 81
 
       solver = SudokuSolver.new(File.expand_path('../grids/maman.sdk', File.dirname(__FILE__)))
