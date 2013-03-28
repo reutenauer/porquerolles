568e87cb596506fac0097df9f14c4a5bf5d17cb1
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-03-28 13:19:31 +0000

    Rspec config: making my life simpler.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,7 @@ require File.expand_path('../lib/sudoku', File.dirname(__FILE__))
 def open_grid(file)
   File.expand_path(File.join('../grids', file), File.dirname(__FILE__))
 end
+
+RSpec.configure do |config|
+  config.filter_run :focus => true
+end
