118ffa38b4dd5001f9580442ae1bdf50378ff0a0
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-03-28 12:14:45 +0000

    More main specs.

--- spec/sudoku/main_spec.rb
+++ spec/sudoku/main_spec.rb
@@ -1,12 +1,30 @@
-def run(file)
+# encoding: UTF-8
+
+def run(file, options = "")
   sudokubin = File.expand_path('../../../bin/sudoku', __FILE__)
   griddir = File.expand_path('../../../grids', __FILE__)
 
-  `#{sudokubin} #{File.join(griddir, file)}` # Mutes command
+  `#{sudokubin} #{options} #{File.join(griddir, file)}` # Mutes command
 end
 
 describe "Main routine" do
-  it "parses arguments in an awkward way" do
+  it "runs main without any switch" do
     run('simple.sdk')
+  end
+
+  it "runs main with the “chains” switch" do
+    run('guardian/2423.sdk', '-c')
+  end
+
+  it "runs main with the “guess” switch" do
+    run('maman.sdk', '-g')
+  end
+
+  it "runs main with both the “chains” and the “guess” switch" do
+    run('misc/X-wing.sdk', '-c -g')
+  end
+
+  it "runs main with the “singles” and the “chains” switch" do
+    run('misc/X-wing.sdk', '-s -c')
   end
 end
