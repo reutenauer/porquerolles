8b433ab221c5debb97ca8ad671bca401b4d9c9e1
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-03-28 12:10:21 +0000

    Make call less awkward.

--- spec/sudoku/main_spec.rb
+++ spec/sudoku/main_spec.rb
@@ -1,8 +1,12 @@
-sudokubin = File.expand_path('../../../bin/sudoku', __FILE__)
-griddir = File.expand_path('../../../grids', __FILE__)
+def run(file)
+  sudokubin = File.expand_path('../../../bin/sudoku', __FILE__)
+  griddir = File.expand_path('../../../grids', __FILE__)
+
+  `#{sudokubin} #{File.join(griddir, file)}` # Mutes command
+end
 
 describe "Main routine" do
   it "parses arguments in an awkward way" do
-    `#{sudokubin} #{griddir}/simple.sdk` # Mutes command
+    run('simple.sdk')
   end
 end
