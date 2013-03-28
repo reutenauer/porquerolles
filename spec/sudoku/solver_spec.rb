122f077057d6427bd7a47d208df0379d563b1e23
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-03-28 12:19:44 +0000

    Split one spec, and tagged one of the new as slow.

--- spec/sudoku/solver_spec.rb
+++ spec/sudoku/solver_spec.rb
@@ -2,14 +2,16 @@ require 'spec_helper'
 
 describe SudokuSolver do
   describe "#solve" do
-    it "solves a couple of grids" do
+    it "solves an easy grid" do
       solver = SudokuSolver.new(open_grid('guardian/2423.sdk'))
       # Not sure whether to test that.
       # expect { solver.solve }.to change(solver, :nb_cell_solved) by(57)
       solver.solve
       # TODO Matcher for that!
       solver.nb_cell_solved.should == 81
+    end
 
+    it "solves a hard grid", :slow => true do
       solver = SudokuSolver.new(open_grid('maman.sdk'))
       solver.solve
       solver.nb_cell_solved.should == 24
