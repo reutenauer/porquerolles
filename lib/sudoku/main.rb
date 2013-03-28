1782c50b1044ad81d50cb372cf298b3045f02f4c
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-03-28 12:33:26 +0000

    Splitting main.rb.

--- lib/sudoku/main.rb
+++ lib/sudoku/main.rb
@@ -26,32 +26,3 @@ class Main
     end
   end
 end
-
-class Options
-  def self.parse(args)
-    params = { }
-
-    # TODO Play with optparse now.
-    # TODO “$0 -f grid/maman.sdk” freezes
-    while args.count > 1
-      f = args.first
-
-      if f == '-s'
-        params[:singles] = true
-        args = args[1..-1]
-      elsif f == '-d'
-        params[:method] = :deduction
-        args = args[1..-1]
-      elsif f == '-c'
-        params[:chains] = true
-        args = args[1..-1]
-      elsif f == '-g'
-        params[:method] = :guess
-        args = args[1..-1]
-      # No tree yet.
-      end
-    end
-
-    params
-  end
-end
