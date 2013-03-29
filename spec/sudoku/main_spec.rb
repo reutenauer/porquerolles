ef269019e13cb2b31cca9c79ae4919b6cfeb19e5
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-03-29 20:02:21 +0000

    Command line tests are not useless in RSpec (redundant with Cucumber).

--- spec/sudoku/main_spec.rb
+++ spec/sudoku/main_spec.rb
@@ -11,37 +11,5 @@ module Sudoku
         Main.run([File.join(griddir, 'simple.sdk')])
       end
     end
-
-    describe "Command line calls" do
-      def run(file, options = "")
-        sudokubin = File.expand_path('../../../bin/sudoku', __FILE__)
-
-        system("#{sudokubin} #{options} #{File.join(griddir, file)} >/dev/null").should be_true
-      end
-
-      it "runs main without any switch" do
-        run('simple.sdk')
-      end
-
-      it "runs main with the “chains” switch" do
-        run('guardian/2423.sdk', '-c')
-      end
-
-      it "runs main with the “guess” switch", :slow => true do
-        run('maman.sdk', '-g')
-      end
-
-      it "runs main with both the “chains” and the “guess” switch" do
-        run('misc/X-wing.sdk', '-c -g')
-      end
-
-      it "runs main with the “singles” and the “chains” switch" do
-        run('misc/X-wing.sdk', '-s -c')
-      end
-
-      it "runs main with only the “chains” switch" do
-        run('misc/X-wing.sdk', '-c')
-      end
-    end
   end
 end
