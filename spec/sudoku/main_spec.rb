934d7c5a53802ee53179844f4462d3a630bfa9ac
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-03-29 10:59:57 +0000

    Spec for it.

--- spec/sudoku/main_spec.rb
+++ spec/sudoku/main_spec.rb
@@ -1,34 +1,47 @@
 # encoding: UTF-8
 
-def run(file, options = "")
-  sudokubin = File.expand_path('../../../bin/sudoku', __FILE__)
-  griddir = File.expand_path('../../../grids', __FILE__)
+require 'spec_helper'
 
-  system("#{sudokubin} #{options} #{File.join(griddir, file)}").should be_true
-end
+module Sudoku
+  describe Main do
+    let(:griddir) { File.expand_path('../../../grids', __FILE__) }
 
-describe "Main routine" do
-  it "runs main without any switch" do
-    run('simple.sdk')
-  end
+    describe "Main routine" do
+      it "runs a simple file" do
+        Main.run([File.join(griddir, 'simple.sdk')])
+      end
+    end
 
-  it "runs main with the “chains” switch" do
-    run('guardian/2423.sdk', '-c')
-  end
+    describe "Command line calls" do
+      def run(file, options = "")
+        sudokubin = File.expand_path('../../../bin/sudoku', __FILE__)
 
-  it "runs main with the “guess” switch", :slow => true do
-    run('maman.sdk', '-g')
-  end
+        system("#{sudokubin} #{options} #{File.join(griddir, file)}").should be_true
+      end
 
-  it "runs main with both the “chains” and the “guess” switch" do
-    run('misc/X-wing.sdk', '-c -g')
-  end
+      it "runs main without any switch" do
+        run('simple.sdk')
+      end
 
-  it "runs main with the “singles” and the “chains” switch" do
-    run('misc/X-wing.sdk', '-s -c')
-  end
+      it "runs main with the “chains” switch" do
+        run('guardian/2423.sdk', '-c')
+      end
 
-  it "runs main with only the “chains” switch" do
-    run('misc/X-wing.sdk', '-c')
+      it "runs main with the “guess” switch", :slow => true do
+        run('maman.sdk', '-g')
+      end
+
+      it "runs main with both the “chains” and the “guess” switch" do
+        run('misc/X-wing.sdk', '-c -g')
+      end
+
+      it "runs main with the “singles” and the “chains” switch" do
+        run('misc/X-wing.sdk', '-s -c')
+      end
+
+      it "runs main with only the “chains” switch" do
+        run('misc/X-wing.sdk', '-c')
+      end
+    end
   end
 end
