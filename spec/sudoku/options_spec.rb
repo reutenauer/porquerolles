d14a6f819f90104b90df18cd4487ce5fe17bfb68
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-03-29 14:30:41 +0000

    Test still not passing, making it pending.

--- spec/sudoku/options_spec.rb
+++ spec/sudoku/options_spec.rb
@@ -23,9 +23,10 @@ module Sudoku
         let(:solver) { Solver.new(output) }
 
         it "is verbose" do
-          pending("Need to refactor Solver first.")
+          pending("Need to learn more about expectations.")
           output.should_receive(:puts).with("One more chain, total 19.  Latest chain [6, [[3, 8], [8, 8]], Column 8].  Total length 3.")
-          solver.solve
+          solver.ingest(read_grid_file('misc/X-wing.sdk'))
+          solver.solve(:verbose => true)
         end
       end
     end
