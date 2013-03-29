ad5ff4564dfd44ac15f31aa9686582375ff3ae38
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-03-29 09:53:39 +0000

    ... but actually we need to cheat a little in the spec.

--- spec/sudoku/options_spec.rb
+++ spec/sudoku/options_spec.rb
@@ -5,15 +5,15 @@ module Sudoku
   describe Options do
     describe "option passing" do
       it "passes the verbose option" do
-        Options.parse(['-v'])[:verbose].should be_true
+        Options.parse(['-v', "dummy name"])[:verbose].should be_true
       end
 
       it "passes the quiet option, as “non-verbose”" do
-        Options.parse(['-q'])[:verbose].should be_false
+        Options.parse(['-q', "dummy name"])[:verbose].should be_false
       end
 
       it "passes the quiet option, overriding verbose" do
-        Options.parse(['-v', '-q'])[:verbose].should be_false
+        Options.parse(['-v', '-q', "dummy name"])[:verbose].should be_false
       end
     end
   end
