a5ccf24cbfd52d39ef1070055dc8fa694818c17d
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-03-30 18:32:34 +0000

    Migrating option parsing to optparse.

--- lib/sudoku/options.rb
+++ lib/sudoku/options.rb
@@ -1,35 +1,45 @@
+require 'optparse'
+
 module Sudoku
   class Options
     def self.parse(args)
       params = { }
 
+      OptionParser.new do |opts|
+        opts.banner = "Usage: #{$0} [options] <file name>"
+
+        opts.on('-s', "--[no]-singles", "Place single candidates") do |s|
+          params[:singles] = s
+        end
+
+        opts.on('-d', "--deduction", "Use deduction method") do
+          params[:method] = :deduction
+        end
+
+        opts.on('-c', "--[no-]chains", "Find chains") do |c|
+          params[:chains] = c
+        end
+
+        opts.on('-g', "--guess", "Use guess method") do
+          params[:method] = :guess
+        end
+
+        opts.on('-v', "--[no-]verbose", "Be more verbose") do |v|
+          params[:verbose] = v
+        end
+
+        opts.on('-q', "--[no-]quiet", "Be quieter") do |q|
+          # Note: if we only use the key :verbose, the defaults are correct (i. e., quiet).
+          params[:verbose] = !q
+        end
+
+        # No tree yet.
+
+        opts.parse!(args)
+      end
+
       # TODO Play with optparse now.
       # TODO “$0 -f grid/maman.sdk” freezes
-      while args.count > 1
-        f = args.first
-
-        if f == '-s'
-          params[:singles] = true
-          args.slice!(0)
-        elsif f == '-d'
-          params[:method] = :deduction
-          args.slice!(0)
-        elsif f == '-c'
-          params[:chains] = true
-          args.slice!(0)
-        elsif f == '-g'
-          params[:method] = :guess
-          args.slice!(0)
-        elsif f == '-v'
-          params[:verbose] = true
-          args.slice!(0)
-        elsif f == '-q'
-          # Note: if we only use the key :verbose, the defaults are correct (i. e., quiet).
-          params[:verbose] = false
-          args.slice!(0)
-        # No tree yet.
-        end
-      end
 
       params
     end
