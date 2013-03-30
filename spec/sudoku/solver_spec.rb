0ec2596c0097c004825572f7e1f605b02cb0264f
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-03-30 19:40:16 +0000

    Spec refactor.

--- spec/sudoku/solver_spec.rb
+++ spec/sudoku/solver_spec.rb
@@ -38,32 +38,19 @@ module Sudoku
         solver.parse_options(['-vc'])
         solver.should be_verbose
         solver.should be_chains
+        pending "Ridiculous name"
       end
 
       it "outputs a message when it encounters an unknown options" do
         output.should_receive(:puts).with("Error: invalid option: -f")
         solver.parse_options(['-f'])
       end
-    end
 
-    describe "options in action" do
-      describe "verbose" do
-        let(:output) { double('output').as_null_object }
-        let(:solver) { Solver.new(output) }
-
-        it "is verbose" do
-          solver.parse_options(['-q', '-c'])
-          solver.should_not be_verbose
+      it "outputs extra messages when verbose" do
+        pending "chains does not work yet" do
+          output.should_receive(:puts).with("One more chain, total 19.  Latest chain [6, [[3, 8], [8, 8]], Column 8].  Total length 3.")
+          solver.ingest(read_grid_file('misc/X-wing.sdk'))
           solver.solve(:verbose => true, :chains => true)
-          solver.should_not be_verbose
-        end
-
-        it "outputs extra messages" do
-          pending "chains does not work yet" do
-            output.should_receive(:puts).with("One more chain, total 19.  Latest chain [6, [[3, 8], [8, 8]], Column 8].  Total length 3.")
-            solver.ingest(read_grid_file('misc/X-wing.sdk'))
-            solver.solve(:verbose => true, :chains => true)
-          end
         end
       end
     end
