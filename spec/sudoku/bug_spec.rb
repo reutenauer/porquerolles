6946cfe17ffd486837ce835f28c150a48fb9168d
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-03-31 05:06:28 +0100

    Now I’ve got it.

--- spec/sudoku/bug_spec.rb
+++ spec/sudoku/bug_spec.rb
@@ -6,6 +6,7 @@ module Sudoku
       solver = Solver.new(STDOUT)
       solver.ingest(read_grid_file('sotd/2013-02-05-diabolical.sdk'))
       solver.solve(:method => :guess)
+      solver.should be_solved
     end
   end
 end
