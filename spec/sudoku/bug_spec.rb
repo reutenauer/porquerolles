101ef2ffaa21162278fef99651433abe899e1273
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-03-31 05:41:15 +0100

    A changed of desription is warranted in the spec ;-)

--- spec/sudoku/bug_spec.rb
+++ spec/sudoku/bug_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 module Sudoku
-  describe "Bug ahead!" do
-    it "is buggy" do
+  describe "Bug fixed :-)" do
+    it "is works" do
       solver = Solver.new(STDOUT)
       solver.ingest(read_grid_file('sotd/2013-02-05-diabolical.sdk'))
       solver.solve(:method => :guess)
