d0fe08f3a2d88f217a5517af1dda43694fe3ebd0
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2013-03-31 05:41:58 +0100

    Moving regression test (now it’s one!) to its rightful place.

--- spec/sudoku/solver_spec.rb
+++ spec/sudoku/solver_spec.rb
@@ -93,6 +93,15 @@ module Sudoku
       end
     end
 
+  describe "Bug fixed :-)" do
+    it "is works" do
+      solver = Solver.new(STDOUT)
+      solver.ingest(read_grid_file('sotd/2013-02-05-diabolical.sdk'))
+      solver.solve(:method => :guess)
+      solver.should be_solved
+    end
+  end
+
     describe "#solve" do
       it "sets additional options" do
         solver.should_not be_verbose
